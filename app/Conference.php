560227227d31cfecedde8eeb71d95ebfb62c4279
Frédéric Chiasson <frederic.chiasson@gmail.com>
2015-05-28 19:46:58 -0400

    Added Conference (empty), added description to models

---
+++ app/Conference.php
@@ -0,0 +1,27 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+
+class Conference extends Model {
+
+	use SoftDeletes;
+
+	/*protected $fillable = ['slug', 'title', 'subtitle', 'starts_at', 'ends_at'];*/
+
+  /*public function venue()
+  {
+    return $this->belongsTo('App\Venue');
+  }
+
+  public function administrator()
+  {
+    return $this->belongsTo('App\User', 'administrator_id');
+  }
+
+  public function participants()
+  {
+    return $this->belongsToMany('App\User');
+  }*/
+}
