b78c391c7356457fe8902c1f3ba90d860e860284
David Pearson <p8.david@gmail.com>
2015-06-01 21:14:16 -0400

    Adjust conf models

--- app/Conference.php
+++ app/Conference.php
@@ -6,19 +6,9 @@ class Conference extends Model {
 
     protected $fillable = ['slug', 'title', 'subtitle', 'location', 'starts_at', 'ends_at'];
 
-    public function venue()
-    {
-        return $this->belongsTo('App\Venue');
-    }
-
     public function administrator()
     {
-        return $this->belongsTo('App\User', 'administrator_id');
-    }
-
-    public function participants()
-    {
-        return $this->belongsToMany('App\User');
+        return $this->belongsTo('App\User', 'created_by');
     }
 
 }
