5a17c2df3de55de78fdaa9d08d004c6a3a63c8c7
hervejodoin <hervejodoin@gmail.com>
2015-06-30 18:14:19 -0400

    fooPages + Routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,59 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+ * NOTES
+ *
+ * Currently this route file contains only static routes that link to the page template views.
+ * This is setup this way to start structuring our designs, but will change once the rest is in place.
+ *
+ * The final routes will not call the views directly, but will go through controllers, once those are ready.
+ *
+ * Also the template views will not be called directly, but rather the pages views.
+ * See http://patternlab.io/ to learn about the Atomic Design approach we're implementing.
+ *
+ *                                                        -- Your friendly neighborhood front-end team
+ */
+
+
+/**
+ *  Home routes
+ */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Note: The logged in page will be accessible from the '/' route when user is authenticated.
+// This should be done in a future commit.
+Route::get('/logged-in', function () {
+    return view('atomic.template.logged-in');
+});
+
+/**
+ * Outing routes
+ */
+
+Route::get('/outing/choose', function () {
+    return view('atomic.template.outing.choose');
+});
+
+Route::get('/outing/create', function () {
+    return view('atomic.template.outing.create');
+});
+
+Route::get('/outing/description', function () {
+    return view('atomic.template.outing.description');
+});
+
+Route::get('/outing/own', function () {
+    return view('atomic.template.outing.own');
+});
+
+/**
+ * Search route
+ */
+
+Route::get('/search', function () {
+    return view('atomic.template.outing.own');
+});
+
