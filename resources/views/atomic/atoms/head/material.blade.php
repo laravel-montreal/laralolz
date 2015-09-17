34eef057d2ed8a1139aa93c5d7370f82a903c45c
Benjamin Gonzalez <benjamin.rosell@gmail.com>
2015-09-17 19:34:21 -0400

    The css framework is not loaded from Bower.
    Make sure you do a bower install

--- resources/views/atomic/atoms/head/material.blade.php
+++ resources/views/atomic/atoms/head/material.blade.php
@@ -1,3 +1,3 @@
-<link rel="stylesheet" href="https://storage.googleapis.com/code.getmdl.io/1.0.4/material.indigo-pink.min.css">
-<script src="https://storage.googleapis.com/code.getmdl.io/1.0.4/material.min.js"></script>
+<link rel="stylesheet" href="{{ elixir('css/app.css') }}">
+<script src="{{ elixir('js/all.js') }}"></script>
 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
