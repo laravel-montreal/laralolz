2f6a0e5c92fffcdca78c036a6374685a0434ba1e
Sami Turcotte <samiturcotte@gmail.com>
2015-09-17 00:42:15 -0400

    Add missing use statement

--- app/Http/Controllers/ConferenceController.php
+++ app/Http/Controllers/ConferenceController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\CreateConferenceRequest;
 use App\Managers\ConferenceManagerInterface;
+use App\Managers\OutingManagerInterface;
 
 class ConferenceController extends Controller
 {
